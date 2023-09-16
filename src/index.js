45c3985caefe1c10db2be103247fde75f763b5eb
Rafael Yasis <rafaelyasis@gmail.com>
2023-09-16 16:33:12 -0500

    added a reducer for each of the forms. This will take in each input and store it to add to the database ??

--- src/index.js
+++ src/index.js
@@ -8,17 +8,42 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 // Reducers
-const feedbackList = (state = [], action) => {
-    if (action.type === 'SET_BOOK_LIST') {
+const feelings = (state = 0, action) => {
+    if (action.type === 'SET_FEELINGS') {
         return action.payload
     }
     return state;
 };
 
+const understanding = (state = 0, action) => {
+    if (action.type === 'SET_UNDERSTANDING') {
+        return action.payload
+    }
+    return state;
+};
+
+const support = (state = 0, action) => {
+    if (action.type === 'SET_SUPPORT') {
+        return action.payload
+    }
+    return state;
+};
+
+const comments = (state = [], action) => {
+    if (action.type === 'SET_COMMENTS') {
+        return action.payload
+    }
+    return state;
+};
+
+
 // Store
 const reduxStore = createStore(
     combineReducers({
-        feedbackList
+        feelings,
+        understanding,
+        support,
+        comments
     }),
     applyMiddleware(logger)
 );
