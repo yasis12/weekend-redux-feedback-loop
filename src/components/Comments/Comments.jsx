14e9c5e443c95c89d1b49ff5bf493a459eb68184
Rafael Yasis <rafaelyasis@gmail.com>
2023-09-17 12:58:10 -0500

    Completed the Comments page Layout. Next I will be working on connecting each of the pages to the database

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,6 +1,36 @@
+import './Comments.css'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 
 function Comments () {
-    
+    const dispatch = useDispatch();
+    const [newComment, setNewComment] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch({
+            type: 'SET_COMMENTS',
+            payload: newComment
+        })
+    };
+
+    return (
+        <div>
+            <div className='pageNumberContainer'>
+                <h4 className='pageNumber'>4 of 4 pages</h4>
+            </div>
+            
+            <br />
+            <h3>How are you feeling today?</h3>
+        <form className="commentFrom" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Comment" onChange={event => setNewFeelings(event.target.value)}/>
+            <br /> <br />
+        </form>
+        <br />
+       
+        </div>
+    )
 }
 
 export default Comments;
